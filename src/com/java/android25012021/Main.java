83bff23a9c23a0a98b6810fac4f9007b1274766c
phatdroid94 <phatdroid94@gmail.com>
2021-01-27 18:28:09 +0700

    kiểu dữ liệu

---
+++ src/com/java/android25012021/Main.java
@@ -0,0 +1,22 @@
+package com.java.android25012021;
+
+public class Main {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+//        comment code : ctrl + / or /* */
+        /*
+         1 : Kiểu dữ liệu
+         Primitive type (Nguyên thủy)
+            int(Số nguyên) ,
+            float(số thực) , double (số thực ) ,
+            char (Ký tự) , boolean(Luận lý) ...
+            int a = 5;
+            float b = 1.1111111f;
+            double c = 2.22222222;
+            char d = 's';
+            boolean e = true;
+            System.out.print(e);
+         Non-Primitive type(Kiểu đối tượng)
+       */
+    }
+}
