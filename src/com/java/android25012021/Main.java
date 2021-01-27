c3ea3abe4bfb0a227847258394745cdb1f0686a2
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2021-01-27 19:08:10 +0700

    Update Main.java

--- src/com/java/android25012021/Main.java
+++ src/com/java/android25012021/Main.java
@@ -18,19 +18,40 @@ public class Main {
             System.out.print(e);
          Non-Primitive type(Kiểu đối tượng)
          2 : Toán tử
-            int a = 5;
-            int b = 5;
-            int ketqua = a++ - --b - a++ + b-- + a++ - a++ - --b;
-            // 5 - --b - a++ + b-- + a++ - a++ - --b; a = 6 , b = 5
-            // 5 - 4 - a++ + b-- + a++ - a++ - --b; a = 6 , b = 4
-            // 5 - 4 - 6 + b-- + a++ - a++ - --b; a = 7 , b = 4
-            // 5 - 4 - 6 + 4 + a++ - a++ - --b; a = 7 , b = 3
-            // 5 - 4 - 6 + 4 + 7 - a++ - --b; a = 8 , b = 3
-            // 5 - 4 - 6 + 4 + 7 - 8 - 2; a = 9 , b = 2
-            // 1 - 2 + 7 - 8 -2
-            // 6 -8 -2
-            // -4
-            System.out.print(ketqua);
-       */
+        int a = 5;
+        int b = 5;
+        int ketqua = a++ - --b - a++ + b-- + a++ - a++ - --b;
+        // 5 - --b - a++ + b-- + a++ - a++ - --b; a = 6 , b = 5
+        // 5 - 4 - a++ + b-- + a++ - a++ - --b; a = 6 , b = 4
+        // 5 - 4 - 6 + b-- + a++ - a++ - --b; a = 7 , b = 4
+        // 5 - 4 - 6 + 4 + a++ - a++ - --b; a = 7 , b = 3
+        // 5 - 4 - 6 + 4 + 7 - a++ - --b; a = 8 , b = 3
+        // 5 - 4 - 6 + 4 + 7 - 8 - 2; a = 9 , b = 2
+        // 1 - 2 + 7 - 8 -2
+        // 6 -8 -2
+        // -4
+        System.out.print(ketqua);
+        3 : Câu điều kiện
+        if else
+        int a = 10;
+        int b = 5;
+        a > b : In ra biến kết quả có giá trị là 1;
+        a < b : In ra biến kết quả có giá trị là -1;
+        a == b : In ra biến kết quả có giá trị là 0;
+        block scope {} : phạm vi dùng để chứa đoạn lệnh (statements)
+        if (a < 10 && b >= 5){
+            System.out.println("Có");
+        }else{
+            System.out.println("Không");
+        }
+        if (!(a > 10)){
+            System.out.println("A bé hơn bằng 10");
+        }
+
+        */
+        
+
+
+
     }
 }
